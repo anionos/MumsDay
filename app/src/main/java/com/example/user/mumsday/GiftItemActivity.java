009c87cd87e686f1fffbbe4dd9f1e90b59c7ff46
anionos <aniebietonoyom32@gmail.com>
2019-04-21 20:49:55 +0100

    Modifying Next Activity

---
+++ app/src/main/java/com/example/user/mumsday/GiftItemActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.mumsday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GiftItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gift_item);
+    }
+}
--- app/src/main/java/com/example/user/mumsday/MainActivity.java
+++ app/src/main/java/com/example/user/mumsday/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.mumsday;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+    public void click(View view){
+        Intent intent = new Intent(this,GiftItemActivity.class);
+        startActivity(intent);
+
+    }
+
 }
